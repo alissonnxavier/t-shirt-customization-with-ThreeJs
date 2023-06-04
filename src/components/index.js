9c3be26ce872f23a4cf7a533b8876efdcaf08ded
Alisson <alissonalves29@gmail.com>
2023-06-03 23:44:12 -0300

    added canvas and tabs components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from './AIPicker';
+import ColorPicker from './ColorPicker';
+import FilerPicker from './FilerPicker';
+import Tab from './Tab'
+
+export { CustomButton, AIPicker, ColorPicker,FilerPicker,Tab };
