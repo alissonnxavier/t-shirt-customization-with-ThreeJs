60ffd1c7ce82beb21d68cab9d1c470b01d28cfef
Alisson <alissonalves29@gmail.com>
2023-06-04 13:36:55 -0300

    added tabs for customization

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -8,6 +8,33 @@ import state from '../store';
 const CameraRig = ({ children }) => {
 
     const group = useRef();
+    const snap = useSnapshot(state);
+
+    useFrame((state, delta) => {
+        const isBreakPoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        //set the initial position of the model
+        let targetPosition = [-0.4, 0, 2];
+        if (snap.intro) {
+            if (isBreakPoint) targetPosition = [0, 0, 2];
+            if (isMobile) targetPosition = [0, 0.2, 2.5];
+        } else {
+            if (isMobile) targetPosition = [0, 0, 2.5];
+            else targetPosition = [0, 0, 2];
+        }
+
+        //set model camera position
+        easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+        //set the model rotation smoothly
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 5, -state.pointer.x * 0.5, 0],
+            0.25,
+            delta
+        )
+    });
 
     return (
         <group
